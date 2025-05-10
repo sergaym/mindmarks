910de63af0e1d4ee5cd117fe96addefbbac14b57
sergaym <sergioayala.contacto@gmail.com>
2025-05-10 20:24:17 +0200

    [FEAT] Reintroduce Header and Footer in Home Component for Enhanced Layout
    - Added Header and Footer components back into the Home page structure, improving the overall layout and user experience.
    - Wrapped the main content area with a semantic <main> tag to enhance accessibility and maintain a clear document structure.
    - This change reinstates essential navigation and footer elements, setting the stage for a more cohesive application design.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import Hero from "@/components/hero";
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
 
 export default function Home() {
   return (
-    <div className="flex-1 flex flex-col">
-      <Hero />
-    </div>
+    <>
+      <Header />
+      <main className="flex-1 flex flex-col">
+        <Hero />
+      </main>
+      <Footer />
+    </>
   );
 }

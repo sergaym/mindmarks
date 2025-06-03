7b09b86d6aa51b91de01cd7f79824807828c5906
sergaym <sergioayala.contacto@gmail.com>
2025-06-03 07:17:43 +0200

    feat(editor-elements): add new content elements for enhanced editing capabilities
    - Introduced HrElement and HrElementStatic for horizontal rule representation within the editor, improving content structuring options.
    - Added LinkElement and LinkElementStatic to facilitate hyperlink integration, enhancing user navigation and content interactivity.
    - Implemented ParagraphElement and ParagraphElementStatic for flexible text formatting, allowing for better content organization.
    - Introduced TocElement and TocElementStatic to support table of contents functionality, enabling users to navigate through headings efficiently.
    - These additions aim to enrich the editing experience by providing versatile content elements that enhance user engagement and content management within the editor.

---
+++ frontend/src/components/editor/elements/content/index.ts
@@ -0,0 +1,9 @@
+// Content Elements
+export { ParagraphElement } from './paragraph-element';
+export { ParagraphElementStatic } from './paragraph-element-static';
+export { LinkElement } from './link-element';
+export { LinkElementStatic } from './link-element-static';
+export { TocElement } from './toc-element';
+export { TocElementStatic } from './toc-element-static';
+export { HrElement } from './hr-element';
+export { HrElementStatic } from './hr-element-static'; 
